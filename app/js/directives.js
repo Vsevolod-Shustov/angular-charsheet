d33d5521b5310d27ea9f09ae577a0088a571b07c
Vsevolod <Vsevolod-Shustov@users.noreply.github.com>
2014-11-27 14:18:09 +0300

    pie chart

--- app/js/directives.js
+++ app/js/directives.js
@@ -28,62 +28,52 @@ csDirectives.directive('d3js', function(){
     restrict: "A",
     template: '<script src="./js/vendor/d3.v3.min.js"></script>',
     link: function(scope, element, attrs){
-      var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
-                11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
-      var w = 500;
-      var h = 100;
-      var barPadding = 1;
-      var svg = d3.select("#d3js")
-        .append("svg")
-        .attr("width", w)
-        .attr("height", h);
-      svg.selectAll("rect")
-        .data(dataset)
-        .enter()
-        .append("rect")
-        .attr("x", 0)
-        .attr("y", function(d) {
-            return h - d*4;  //Height minus data value
-        })
-        .attr("width", w / dataset.length - barPadding)
-        .attr("height", function(d) {
-            return d*4;  //Just the data value
-        })
-        .attr("x", function(d, i) {
-            return i * (w / dataset.length);
-        })
-        .attr("fill", function(d) {
-            return "rgb(0, 0, " + (d * 10) + ")";
-        });
-      svg.selectAll("text")
-   .data(dataset)
-   .enter()
-   .append("text")
-   .text(function(d) {
-        return d;
-   })
-   .attr("x", function(d, i) {
-        return i * (w / dataset.length) + 5;  // +5
-   })
-   .attr("y", function(d) {
-        return h - (d * 4) + 15;              // +15
-   })
-   .attr("font-family", "sans-serif")
-   .attr("font-size", "11px")
-   .attr("fill", "white")
-   .attr("text-anchor", "middle")
-   .attr("x", function(d, i) {
-        return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
-    })
-    .attr("y", function(d) {
-        return h - (d * 4) + 14;  //15 is now 14
-    });
-      //console.log(scope.character);
-      /*var attributes = [];
+      var data = [];
       angular.forEach(scope.character.attributes, function(attribute){
-        attributes.push(attribute.value);
+        data.push(
+          {
+            "label": attribute.name,
+            "value": attribute.value
+          }
+        );
       });
-      console.log(attributes);*/
+      console.log(data);
+      
+      var w = 300;
+      var h = 300;
+      var r = h/2;
+      var color = d3.scale.category20();
+          
+      var vis = d3.select("#d3js").append("svg:svg").data([data]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + r + ")");
+      var pie = d3.layout.pie().value(function(d){return d.value;});
+
+      // declare an arc generator function
+      var arc = d3.svg.arc().outerRadius(r);
+
+      // select paths, use arc generator to draw
+      var arcs = vis.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
+      arcs.append("svg:path")
+          .attr("fill", function(d, i){
+              return color(i);
+          })
+          .attr("d", function (d) {
+              console.log(arc(d));
+              return arc(d);
+          });
+
+      // add the text
+      arcs.append("svg:text").attr("transform", function(d){
+        d.innerRadius = r/1.5;
+        d.outerRadius = r;
+        return "translate(" + arc.centroid(d) + ")";}).attr("text-anchor", "middle").text( function(d, i) {
+          return data[i].label.slice(0,3).toUpperCase();
+        });
+        
+      arcs.append("svg:text").attr("transform", function(d){
+        d.innerRadius = 0;
+        return "translate(" + arc.centroid(d) + ")";}).attr("text-anchor", "middle").text( function(d, i) {
+          return data[i].value;
+        });
     }
   }
 });
