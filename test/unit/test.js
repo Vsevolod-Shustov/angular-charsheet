fdb4d6161c9f8ceace617dbbd8446efbcb3d1c8f
Vsevolod <Vsevolod-Shustov@users.noreply.github.com>
2014-11-29 13:11:43 +0300

    a couple of simple unit tests

--- test/unit/test.js
+++ test/unit/test.js
@@ -1,20 +1,31 @@
-/*describe('characterCtrl', function(){
+describe('characterCtrl', function(){
   var scope = {};
   beforeEach(module('csApp'));
   beforeEach(inject(function($rootScope, $controller){
-    //localStorage['globalMap'] = JSON.stringify({"0 0":{"x":"0","y":"0","terrain":"plains"}});
     scope = $rootScope.$new();
-    characterCtrl = $controller('characterCtrl'){
-      $scope: scope
-    };
+    $controller('characterCtrl', {$scope: scope});
+    scope.loadCharacter();
   }));
-  it('should make attributes equal 10', function(){
-    expect(scope.character.attributes.strength.value).toBe(10);
+  it('should load attributes', function(){
+    expect(scope.character.attributes.strength.value).toBe(12);
+    expect(scope.character.attributes.dexterity.value).toBe(14);
+    expect(scope.character.attributes.constitution.value).toBe(16);
+    expect(scope.character.attributes.intelligence.value).toBe(18);
+    expect(scope.character.attributes.wisdom.value).toBe(20);
+    expect(scope.character.attributes.charisma.value).toBe(22);
   });
-});*/
-describe("My First Test", function(){
+  it('should calculate attributes\' modifiers', function(){
+    expect(scope.character.attributes.strength.mod).toBe(1);
+    expect(scope.character.attributes.dexterity.mod).toBe(2);
+    expect(scope.character.attributes.constitution.mod).toBe(3);
+    expect(scope.character.attributes.intelligence.mod).toBe(4);
+    expect(scope.character.attributes.wisdom.mod).toBe(5);
+    expect(scope.character.attributes.charisma.mod).toBe(6);
+  });
+});
+/*describe("My First Test", function(){
 
   it("should be true", function(){
       expect(true).toBe(true);
   });
-});
+});*/
